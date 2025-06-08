2b9e889575e595a5f8c3d353540619630cd038a8
jayden <juvbiama@icloud.com>
2025-06-08 16:10:18 -0600

    Meep Meep to roadrunner

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot.java
@@ -0,0 +1,56 @@
+package org.firstinspires.ftc.teamcode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
+//import org.firstinspires.ftc.teamcode.Subsystems.Compass;
+import org.firstinspires.ftc.teamcode.Drive;
+//import org.firstinspires.ftc.teamcode.Subsystems.GoBildaPinpointDriver;
+//import org.firstinspires.ftc.teamcode.Subsystems.Intake;
+//import org.firstinspires.ftc.teamcode.Subsystems.Lift;
+//import org.firstinspires.ftc.teamcode.Subsystems.Odometry;
+
+public class robot {
+    // Public Subsystems
+//    public Intake intake;
+//    public Lift lift;
+    public Drive drive;
+//    public Compass compass;
+//    public Odometry odometry;
+    public Telemetry telemetry;
+
+    public robot(HardwareMap hardwareMap, Telemetry telemetry) {
+        // Private Devices
+        Servo claw = hardwareMap.get(Servo.class, "claw");
+        Servo wrist = hardwareMap.get(Servo.class, "wrist");
+        DcMotor liftMotorLeft = hardwareMap.get(DcMotor.class, "liftMotorLeft");
+        DcMotor liftMotorRight = hardwareMap.get(DcMotor.class, "liftMotorRight");
+        DcMotor shoulder = hardwareMap.get(DcMotor.class, "shoulder");
+        DcMotor frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
+        DcMotor frontRight = hardwareMap.get(DcMotor.class, "frontRight");
+        DcMotor rearLeft = hardwareMap.get(DcMotor.class, "rearLeft");
+        DcMotor rearRight = hardwareMap.get(DcMotor.class, "rearRight");
+
+//        GoBildaPinpointDriver odometryComputer = hardwareMap.get(GoBildaPinpointDriver.class, "odo");
+//
+//        // Initialize Public Subsystems
+//        compass = new Compass(odometryComputer);
+//        intake = new Intake(claw, wrist);
+//        lift = new Lift(liftMotorLeft, liftMotorRight, shoulder);
+//        drive = new Drive(frontLeft, frontRight, rearLeft, rearRight, compass);
+//        odometry = new Odometry(odometryComputer, compass);
+
+        // Public Devices (not subsystems)
+        this.telemetry = telemetry;
+//        this.odometry = hardwareMap.get(GoBildaPinpointDriver.class, "odo");
+//        this.odometry.setEncoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_SWINGARM_POD);
+//        this.odometry.setOffsets(-6.44, 6.8745);
+//        this.odometry.setEncoderDirections(GoBildaPinpointDriver.EncoderDirection.REVERSED, GoBildaPinpointDriver.EncoderDirection.FORWARD);
+//        this.odometry.resetPosAndIMU();
+//        this.odometry.setPosition(new Pose2D(DistanceUnit.INCH, 0, 0, AngleUnit.DEGREES, 0));
+    }
+}
