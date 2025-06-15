986c9e6669e42a08b60c266c1264e47d59747c66
Aarav Sharma <iamcoder18@gmail.com>
2025-06-14 22:07:25 -0600

    refactor: remove comments from PIDTuning.java
    - Removed commented out code related to old Arm class and Lift class.
    - Simplified the loop method by directly calling `arm.Goto(target)` instead of conditional checks.
    - Removed unused telemetry updates for arm speed and PIDF values.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PIDF/PIDTuning.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PIDF/PIDTuning.java
@@ -14,48 +14,17 @@ import org.firstinspires.ftc.robotcore.external.Telemetry;
 @Config
 @Autonomous
 public class PIDTuning extends OpMode {
-//    Arm arm;
     ArmNew arm;
     public static int target = 0;
-//    Lift lift;
-
     MultipleTelemetry multipleTelemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
 
     @Override
     public void init() {
         arm = new ArmNew(hardwareMap,multipleTelemetry);
-        //telemetry.addLine("Hello from PIDTuning");
-//        lift = new Lift(hardwareMap);
     }
 
     @Override
     public void loop() {
-//        if (gamepad1.a) {
-//            CommandScheduler.getInstance().schedule(
-//                    new SequentialCommandGroup(
-//                            arm.goTo(target).withTimeout(3000)
-//                    )
-//            );
-
-            arm.Goto(target);
-//        }
-//        else if (gamepad1.b) {
-////            CommandScheduler.getInstance().schedule(
-////                    new SequentialCommandGroup(
-////                            arm.goTo(0).withTimeout(3000)
-////                    )
-////            );
-//            arm.Goto(0);
-//        }
-
-//        Arm.ArmTelemetry armTelemetry = arm.telemetry();
-//        telemetry.addData("Arm Speed", armTelemetry.getSpeed());
-//        telemetry.addData("Arm Current Position", armTelemetry.getCurrentPosition());
-//        telemetry.addData("Arm Target", armTelemetry.getTarget());
-//        telemetry.addData("Arm P", armTelemetry.getP());
-//        telemetry.addData("Arm I", armTelemetry.getI());
-//        telemetry.addData("Arm D", armTelemetry.getD());
-//        telemetry.addData("Arm F", armTelemetry.getF());
-//        telemetry.update();
+        arm.Goto(target);
     }
 }
