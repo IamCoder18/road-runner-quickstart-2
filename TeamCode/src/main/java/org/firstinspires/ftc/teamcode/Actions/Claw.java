bf43017418c1cf42858cce857e14242c591a0169
Aarav Sharma <iamcoder18@gmail.com>
2025-06-15 14:41:34 -0600

    feat: implement Claw action
    This commit implements the `Claw` action, which controls both the claw and wrist servos.
    
    The `run` method now sets the position of both servos to the `target` value provided in the constructor. Telemetry for the target positions of both servos has also been added.
    
    Additionally, the servo ranges for both the claw and wrist have been scaled to -1 to 1.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Actions/Claw.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Actions/Claw.java
@@ -9,15 +9,31 @@ import com.qualcomm.robotcore.hardware.Servo;
 
 public class Claw implements Action {
 	private final Servo claw;
+	private final Servo wrist;
 	private final double target;
 
 	public Claw(HardwareMap hw, double target) {
 		this.claw = hw.get(Servo.class, "claw");
+		this.wrist = hw.get(Servo.class, "wrist");
 		this.target = target;
+
+		this.claw.scaleRange(-1, 1);
+		this.wrist.scaleRange(-1, 1);
 	}
 
+	/**
+	 * Moves the claw and wrist to the target position depending on the targets given in the
+	 * constructor. Unfortunately, you can't get the servo positions in the FTC SDK, so you
+	 * need to add a short wait after every move to ensure the servos have time to move.
+	 */
 	@Override
 	public boolean run(@NonNull TelemetryPacket telemetryPacket) {
-		return false;
+		this.claw.setPosition(target);
+		this.wrist.setPosition(target);
+
+		telemetryPacket.put("Claw Target", target);
+		telemetryPacket.put("Wrist Target", target);
+
+		return true;
 	}
 }
