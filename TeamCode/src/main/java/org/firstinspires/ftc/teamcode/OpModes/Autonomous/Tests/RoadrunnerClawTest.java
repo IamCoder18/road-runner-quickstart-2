e04bf03896b16ae7e2e6fac83365e35c5d2c2707
Aarav Sharma <iamcoder18@gmail.com>
2025-06-15 15:08:52 -0600

    RoadrunnerClawTest: add Wrist Action Refactor: move Action-related methods into new Reset class
    - Added Wrist Action to RoadrunnerClawTest to test wrist functionality.
    - Created Reset class to encapsulate methods for resetting encoders and run modes for lift and shoulder motors. This improves code organization and reusability.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Autonomous/Tests/RoadrunnerClawTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Autonomous/Tests/RoadrunnerClawTest.java
@@ -2,16 +2,18 @@ package org.firstinspires.ftc.teamcode.OpModes.Autonomous.Tests;
 
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.SleepAction;
 import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.PIDFCoefficients;
 
 import org.firstinspires.ftc.teamcode.Actions.Claw;
+import org.firstinspires.ftc.teamcode.Actions.Wrist;
 
 @Autonomous
 public class RoadrunnerClawTest extends OpMode {
-	PIDFCoefficients pidfCoefficients = new PIDFCoefficients(0.025, 0, 0, 0.0006);
 	MultipleTelemetry multipleTelemetry;
 
 	@Override
@@ -25,7 +27,11 @@ public class RoadrunnerClawTest extends OpMode {
 	@Override
 	public void start(){
 		Actions.runBlocking(
-				new Claw(hardwareMap, 1)
+				new SequentialAction(
+					new Claw(hardwareMap, 1),
+					new SleepAction(3),
+					new Wrist(hardwareMap, 1)
+				)
 		);
 	}
 
